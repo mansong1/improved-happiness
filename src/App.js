80406d3c7b4a96b08b31993ff309a5e8af89d928
Martin Ansong <martin.ansong@protonmail.com>
2021-07-01 20:00:22 +0100

    Add hand detection function

--- src/App.js
+++ src/App.js
@@ -10,9 +10,43 @@ function App() {
   const canvasRef = useRef(null);
 
   const loadHandpose = async () => {
-    const net = await handpose.load()
-    console.log('Handpose model loaded.')
-  }
+    const net = await handpose.load();
+    console.log('Handpose model loaded.');
+
+    // Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 100)
+  };
+  
+  const detect = async (net) => {
+    if (
+        typeof webcamRef.current !=='undefined' && 
+        webcamRef.current !== null && 
+        webcamRef.current.video.readyState === 4  // Check we are receiving data
+      ){
+        
+          // Get video properties
+         const video = webcamRef.current.video;
+         const videoWidth = video.videoWidth;
+         const videoHeight = video.videoHeight;
+
+         // Set video height and width
+         video.width = videoWidth;
+         video.height = videoHeight;
+
+        // Set canvas height and width
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+
+        // Make Detections
+        const hand = await net.estimateHands(video);
+        console.log(hand);
+
+        // Draw mesh
+
+      }
+  };
 
   loadHandpose();
 
