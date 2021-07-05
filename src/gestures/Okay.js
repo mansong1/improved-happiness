4dd4d0a71b4844f7675a66d8d9e6cc6356d993cf
Martin Ansong <martin.ansong@protonmail.com>
2021-07-05 20:19:42 +0100

    fix okay gesture detection

--- src/gestures/Okay.js
+++ src/gestures/Okay.js
@@ -1,12 +1,12 @@
-import {Finger, FingerCurl, FingerDirection, GestureDescription} from 'fingerpose'; 
+import {Finger, FingerCurl, FingerDirection, GestureDescription} from 'fingerpose';
 
 const okayGesture = new GestureDescription('okay');
 
-okayGesture.addCurl(Finger.Index, FingerCurl.FullCurl, 1);
-okayGesture.addCurl(Finger.Thumb, FingerCurl.FullCurl, 1);
+
+okayGesture.addCurl(Finger.Thumb, FingerCurl.FullCurl, 1.0);
+okayGesture.addCurl(Finger.Index, FingerCurl.FullCurl, 1.0);
 
 for (let finger of [Finger.Pinky, Finger.Ring, Finger.Middle]) {
-  okayGesture.addCurl(finger, FingerCurl.HalfCurl, 0.75);
   okayGesture.addDirection(finger, FingerDirection.DiagonalUpRight, 0.5);
   okayGesture.addDirection(finger, FingerDirection.DiagonalUpLeft, 0.5);
   okayGesture.addDirection(finger, FingerDirection.VerticalUp, 0.5);
