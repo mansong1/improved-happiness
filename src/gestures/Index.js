8946ad9af4b31749ac81f6ae8dfcbda45d72a7fb
Martin Ansong <martin.ansong@protonmail.com>
2021-07-05 15:48:37 +0100

    export all gestures

--- src/gestures/Index.js
+++ src/gestures/Index.js
@@ -1,5 +1,12 @@
 import ThumbsDownGesture from './ThumbsDown';
+import HelloGesture from './Hello';
+import IndexGesture from './OneCount';
+import OkayGesture from './Okay';
+
 
 export {
+    OkayGesture,
+    IndexGesture,
+    HelloGesture,
     ThumbsDownGesture,
 }
