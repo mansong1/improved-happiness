c39a2c6e348185e4c2c073ec3b5fce4296446013
Martin Ansong <martin.ansong@protonmail.com>
2021-07-05 19:14:58 +0100

    Fix hello confidence

--- src/gestures/Hello.js
+++ src/gestures/Hello.js
@@ -2,16 +2,20 @@ import {Finger, FingerCurl, FingerDirection, GestureDescription} from 'fingerpos
 
 const helloGesture = new GestureDescription('hello');
 
+// Index
+helloGesture.addCurl(Finger.Index, FingerCurl.NoCurl, 1.0);
+helloGesture.addDirection(Finger.Index, FingerDirection.VerticalUp, 1.0);
+
 // Thumb
-helloGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 0.5);
-helloGesture.addDirection(Finger.Thumb, FingerDirection.DiagonalUpLeft, 0.5);
-helloGesture.addDirection(Finger.Thumb, FingerDirection.DiangonalUpRight, 0.5);
+helloGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0);
+helloGesture.addDirection(Finger.Thumb, FingerDirection.VerticalUp, 1.0);
 
 // Other Fingers
 for(let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]){
-    helloGesture.addCurl(finger, FingerCurl.NoCurl, 0.8);
+    helloGesture.addCurl(finger, FingerCurl.NoCurl, 1.0);
     helloGesture.addDirection(finger, FingerDirection.DiagonalUpLeft, 0.7);
     helloGesture.addDirection(finger, FingerDirection.DiagonalUpRight, 0.7);
+    helloGesture.addDirection(finger, FingerDirection.VerticalUp, 1.0);
 }
 
 export default helloGesture;
