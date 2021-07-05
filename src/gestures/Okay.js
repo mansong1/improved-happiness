e8bfdf5a1a44a947c3d5a55b3ade645ee1fb00d4
Martin Ansong <martin.ansong@protonmail.com>
2021-07-05 15:56:12 +0100

    remove call to fp

--- src/gestures/Okay.js
+++ src/gestures/Okay.js
@@ -2,14 +2,14 @@ import {Finger, FingerCurl, FingerDirection, GestureDescription} from 'fingerpos
 
 const okayGesture = new GestureDescription('okay');
 
-okayGesture.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1);
-okayGesture.addCurl(fp.Finger.Thumb, fp.FingerCurl.FullCurl, 1);
+okayGesture.addCurl(Finger.Index, FingerCurl.FullCurl, 1);
+okayGesture.addCurl(Finger.Thumb, FingerCurl.FullCurl, 1);
 
-for (let finger of [fp.Finger.Pinky, fp.Finger.Ring, fp.Finger.Middle]) {
-  okayGesture.addCurl(finger, fp.FingerCurl.HalfCurl, 0.75);
-  okayGesture.addDirection(finger, fp.FingerDirection.DiagonalUpRight, 0.5);
-  okayGesture.addDirection(finger, fp.FingerDirection.DiagonalUpLeft, 0.5);
-  okayGesture.addDirection(finger, fp.FingerDirection.VerticalUp, 0.5);
+for (let finger of [Finger.Pinky, Finger.Ring, Finger.Middle]) {
+  okayGesture.addCurl(finger, FingerCurl.HalfCurl, 0.75);
+  okayGesture.addDirection(finger, FingerDirection.DiagonalUpRight, 0.5);
+  okayGesture.addDirection(finger, FingerDirection.DiagonalUpLeft, 0.5);
+  okayGesture.addDirection(finger, FingerDirection.VerticalUp, 0.5);
 }
 
 export default okayGesture;
