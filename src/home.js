6cb4c90966238945d97f91bd374cac7f2c5c2e74
Martin Ansong <martin.ansong@protonmail.com>
2021-09-04 13:11:43 +0100

    Add sample form

--- src/home.js
+++ src/home.js
@@ -1,4 +1,27 @@
+import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 import { Background, Form, GlobalStyle, InputsWrapper, InputsWrapperItem, Input } from "./styles";
+
+const Home = () => {
+  return (
+    <Background>
+      <GlobalStyle />
+      <Form>
+        <InputsWrapper>
+          <InputsWrapperItem>
+            <FontAwesomeIcon icon={faUser} />
+            <Input placeholder="Username" />
+          </InputsWrapperItem>
+          <InputsWrapperItem>
+            <FontAwesomeIcon icon={faUser} />
+            <Input placeholder="Password" />
+          </InputsWrapperItem>
+        </InputsWrapper>
+      </Form>
+    </Background>
+  );
+};
+
+export default Home;
