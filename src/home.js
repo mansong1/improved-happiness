2324c06ba63c2fbf47e21737f9e304d865db3b20
Martin Ansong <martin.ansong@protonmail.com>
2021-09-04 12:50:23 +0100

    import input

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-import { Background, Form, GlobalStyle, InputsWrapper, InputsWrapperItem } from "./styles";
+import { Background, Form, GlobalStyle, InputsWrapper, InputsWrapperItem, Input } from "./styles";
