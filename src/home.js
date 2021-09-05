c53da3ea7427bfc9722310da542062014f19d84c
Martin Ansong <martin.ansong@protonmail.com>
2021-09-05 15:12:33 +0100

    update home.js

--- src/home.js
+++ src/home.js
@@ -1,55 +1,56 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-import { 
-  Background, 
-  GlobalStyle,
-  Form,
-  FormTitle,
-  InputsWrapper,
-  InputsWrapperItem,
-  Input,
-  SubmitButton,
-} from "./styles";
+import {
+    GlobalStyle,
+    Background,
+    Form,
+    FormTitle,
+    InputsWrapper,
+    InputsWrapperItem,
+    Input,
+    SubmitButton,
+  } from "./styles";
 
-function Home(){
-  
-  const history = useHistory();
-
-  const handleClick = () => {
-    history.push('/gesture-recognition');
-  };
-
+function Home() {
   return (
     <React.Fragment>
-      <GlobalStyle />
+    <GlobalStyle />
     <Background>
-      <Form>
-        <InputsWrapper>
-          <InputsWrapperItem>
-            <FontAwesomeIcon icon={faUser} />
-            <Input placeholder="First Name" />
-          </InputsWrapperItem>
-          <InputsWrapperItem>
-            <FontAwesomeIcon icon={faUser} />
-            <Input placeholder="Last Name" />
-          </InputsWrapperItem>
-          <InputsWrapperItem>
-            <FontAwesomeIcon icon={faEnvelope} />
-            <Input placeholder="Email" />
-          </InputsWrapperItem>
-          <InputsWrapperItem noUnderline={true}>
-            <SubmitButton onClick={handleClick}>
-              Submit
-              </SubmitButton>
-          </InputsWrapperItem>
-        </InputsWrapper>
-      </Form>
+        <Form>
+            <InputsWrapper>
+            <InputsWrapperItem>
+                <FontAwesomeIcon icon={faUser} />
+                <Input type="text" required placeholder="First Name" />
+            </InputsWrapperItem>
+            <InputsWrapperItem>
+                <FontAwesomeIcon icon={faUser} />
+                <Input type="text" required placeholder="Last Name" />
+            </InputsWrapperItem>
+            <InputsWrapperItem>
+                <FontAwesomeIcon icon={faEnvelope} />
+                <Input type="email" required placeholder="Email" />
+            </InputsWrapperItem>
+            <InputsWrapperItem noUnderline={true}>
+                <SubmitButton type="submit" >
+                Submit
+                </SubmitButton>
+            </InputsWrapperItem>
+            </InputsWrapper>
+        </Form>
     </Background>
     </React.Fragment>
   );
-};
+}
 
 export default Home;
+
+// import FormDataComponent from './components/form_data.component';
+
+// function Home(){
+
+//   return (
+//       <FormDataComponent/>
+//   );
+// };
