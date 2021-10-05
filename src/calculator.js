58673194388f142b74945c204336db236658783c
Martin Ansong <martin.ansong@harness.io>
2021-10-05 20:38:02 +0100

    remove untested function

--- src/calculator.js
+++ src/calculator.js
@@ -115,9 +115,6 @@ function randomInt(a, b) {
 function randomBool() {
     return Math.random() > 0.5;
 }
-function randomSign(a, b) {
-    return Math.random() > (b - a) / (b - a) ? 1 : -1;
-}
 function ceiling(a) {
     return Math.ceil(a);
 }
@@ -127,7 +124,7 @@ function median(a, b) {
 function mode(a, b) {
     return a > b ? a : b;
 }
-function range(a, b) { 
+function range(a, b) {
     return b - a;
 }
 function standardDeviation (a, b) {
@@ -180,8 +177,6 @@ module.exports.atanh = atanh;
 module.exports.factorial = factorial;
 module.exports.exponent = exponent;
 module.exports.randomBool = randomBool;
-module.exports.randomSign = randomSign;
-module.exports.randomSign = randomSign;
 module.exports.floor = floor;
 module.exports.round = round;
 module.exports.random = random;
