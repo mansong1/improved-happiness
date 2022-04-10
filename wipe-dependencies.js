878cacbbcf0be24ce4d22f3ac64b8099c4d46317
Martin Ansong <martin.ansong@harness.io>
2022-04-10 14:11:10 +0100

    use const instead of var

--- wipe-dependencies.js
+++ wipe-dependencies.js
@@ -2,12 +2,12 @@ const fs = require('fs')
 const wipeDependencies = () => {
   const file  = fs.readFileSync('package.json')
   const content = JSON.parse(file)
-  for (var devDep in content.devDependencies) {
+  for (const devDep in content.devDependencies) {
     if (content.devDependencies[devDep].match(/\W+\d+.\d+.\d+-?((alpha|beta|rc)?.\d+)?/g)) {
       content.devDependencies[devDep] = '*';
     }
   }
-  for (var dep in content.dependencies) {
+  for (const dep in content.dependencies) {
     if (content.dependencies[dep].match(/\W+\d+.\d+.\d+-?((alpha|beta|rc)?.\d+)?/g)) {
       content.dependencies[dep] = '*';
     }
