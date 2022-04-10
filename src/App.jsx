652dd3811a63ff0db5aa8bc3f02256bcda914a2e
Martin Ansong <martin.ansong@harness.io>
2022-04-10 12:28:51 +0100

    update app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Home from "./Home";
+import GestureRecognition from "./GestureRecognition";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+	return (
+		<Router>
+			<Routes>
+				<Route
+					exact
+					path="/"
+					element={
+						<Home url="https://mansong.ff-demo.co.uk/gesture-recognition" />
+					}
+				/>
+				<Route
+					exact
+					path="/gesture-recognition"
+					element={<GestureRecognition />}
+				/>
+			</Routes>
+		</Router>
+	);
+}
+
+export default App;
